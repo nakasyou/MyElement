31aff911b63d678357bcc02ff3e1880e0926c090
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-08-11 12:43:05 +0900

    Update main.ts

--- deno/main.ts
+++ deno/main.ts
@@ -1,5 +1,15 @@
 import { Hono } from 'https://deno.land/x/hono/mod.ts'
+import { serveStatic } from 'https://deno.land/x/hono/middleware.ts'
 
 const app = new Hono()
-
+app.use('/*', async (c, next) => {
+  c.header('X-Frame-Options', 'SAMEORIGIN')
+  c.header('frame-ancestors', "'self'")
+  c.header('X-Content-Type-Options', 'nosniff')
+  c.header('X-XSS-Protection', '1; mode=block;')
+  await next()
+})
+app.get('/*', serveStatic({
+  root: './'
+}))
 Deno.serve(app.fetch)
